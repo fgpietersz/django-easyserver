e10c7aee61c973740ac23ce5ae0787996155ad80
Graeme Pietersz <graeme@pietersz.net>
2023-05-21 20:04:57 +0100

    Return static files from STATIC_ROOT

--- easyserver/management/commands/run_easyserver.py
+++ easyserver/management/commands/run_easyserver.py
@@ -1,7 +1,27 @@
-from cheroot.wsgi import Server
+from pathlib import Path
+
+from cheroot.wsgi import Server, PathInfoDispatcher
 
 from django.core.management.base import BaseCommand
 from django.core.wsgi import get_wsgi_application
+
+from django.conf import settings
+
+
+# directory as a Path object
+static_root = Path(settings.STATIC_ROOT)
+
+
+def serve_static(environ, start_response):
+    """A simple WSGI application to return a static file"""
+    # FIXME - this is insecure and requires path check to prevent ../ etc
+    full_path = static_root / environ['PATH_INFO'].lstrip('/')
+    # TODO deal with directories and 404
+    with open(full_path, 'rb') as f:
+        content = f.read()
+    # TODO need to return content types
+    start_response('200 OK', [])
+    return [content]
 
 
 class Command(BaseCommand):
@@ -10,6 +30,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         server = Server(
             ('127.0.0.1', 8080),
-            get_wsgi_application()
+            PathInfoDispatcher(
+                {
+                    '/' + settings.STATIC_URL.lstrip('/'): serve_static,
+                    '/': get_wsgi_application()
+                }
+            )
         )
         server.start()
