11d8041f7b130bda4d68a2d4f2d5142956c20f29
Graeme Pietersz <graeme@pietersz.net>
2023-05-21 22:00:14 +0100

    Generalise to serve multiple dirs - media and static

--- easyserver/management/commands/run_easyserver.py
+++ easyserver/management/commands/run_easyserver.py
@@ -12,16 +12,21 @@ from django.conf import settings
 static_root = Path(settings.STATIC_ROOT)
 
 
-def serve_static(environ, start_response):
-    """A simple WSGI application to return a static file"""
-    # FIXME - this is insecure and requires path check to prevent ../ etc
-    full_path = static_root / environ['PATH_INFO'].lstrip('/')
-    # TODO deal with directories and 404
-    with open(full_path, 'rb') as f:
-        content = f.read()
-    # TODO need to return content types
-    start_response('200 OK', [])
-    return [content]
+def get_static_app(root):
+    """Return a WSGI app that serves static files from root"""
+    root_obj = Path(root)
+
+    def serve_static(environ, start_response):
+        # FIXME - this is insecure and requires path check to prevent ../ etc
+        print(environ['PATH_INFO'])
+        full_path = root_obj / environ['PATH_INFO'].lstrip('/')
+        # TODO deal with directories and 404
+        with open(full_path, 'rb') as f:
+            content = f.read()
+        # TODO need to return content types
+        start_response('200 OK', [])
+        return [content]
+    return serve_static
 
 
 class Command(BaseCommand):
@@ -32,7 +37,10 @@ class Command(BaseCommand):
             ('127.0.0.1', 8080),
             PathInfoDispatcher(
                 {
-                    '/' + settings.STATIC_URL.lstrip('/'): serve_static,
+                    '/' + settings.STATIC_URL.lstrip('/'):
+                        get_static_app(settings.STATIC_ROOT),
+                    '/' + settings.MEDIA_URL.lstrip('/'):
+                        get_static_app(settings.MEDIA_ROOT),
                     '/': get_wsgi_application()
                 }
             )
