3282b179603bbc4811ada5f76a8e364e41555a7e
Graeme Pietersz <graeme@pietersz.net>
2023-05-22 23:20:17 +0100

    Replace PathInfoDispatcher with custom dispatcher

--- easyserver/management/commands/run_easyserver.py
+++ easyserver/management/commands/run_easyserver.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+import re
 
-from cheroot.wsgi import Server, PathInfoDispatcher
+from cheroot.wsgi import Server
 
 from django.core.management.base import BaseCommand
 from django.core.wsgi import get_wsgi_application
@@ -27,20 +28,38 @@ def get_static_app(root):
     return serve_static
 
 
+def get_dispatcher():
+    # paths can only be top level dir
+    static_path = settings.STATIC_URL.strip('/').split('/')
+    assert len(static_path) == 1
+    media_path = settings.MEDIA_URL.strip('/').split('/')
+    assert len(media_path) == 1
+    path_map = {
+        static_path[0]: get_static_app(settings.STATIC_ROOT),
+        media_path[0]: get_static_app(settings.MEDIA_ROOT),
+    }
+    paths = {static_path[0], media_path[0]}
+    if hasattr(settings, 'WELL_KOWN_ROOT'):
+        paths.add('.well_known')
+        path_map['.well_known/'] = get_static_app(settings.WELL_KOWN_ROOT)
+    app = get_wsgi_application()
+
+    def dispatch(environ, start_response):
+        split_path = environ['PATH_INFO'].strip('/').split('/')
+        if not (split_path and (split_path[0] in paths)):
+            return app(environ, start_response)
+        assert len(split_path) >= 2
+        return path_map[split_path[0]]('/'.join(split_path[1:]), start_response)
+    return dispatch
+
+
 class Command(BaseCommand):
     help = 'Run a production quality server'
 
     def handle(self, *args, **options):
         server = Server(
             ('127.0.0.1', 8080),
-            PathInfoDispatcher(
-                {
-                    '/' + settings.STATIC_URL.lstrip('/'):
-                        get_static_app(settings.STATIC_ROOT),
-                    '/' + settings.MEDIA_URL.lstrip('/'):
-                        get_static_app(settings.MEDIA_ROOT),
-                    '/': get_wsgi_application()
-                }
-            )
+            get_dispatcher()
+            # TODO Add other args
         )
         server.start()
